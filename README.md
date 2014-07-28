54e7782b5f124506a727675eb25ef47e27f7c1c9
beartums <eric@griffithnet.com>
2014-07-28 15:48:58 -0400

    readme and unload method

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ node-exiftool
 =============
 
 Javascript wrapper for exiftool, using stay_open for speed with multi-file requests
+
+##Requires
+exiftool executable.  You can point to a lacally-installed version with the ```load```
+method, otherwise this wrapper will look for it in './vendor/exiftool/exiftool.exe'.
+
+##Methods
+
+###.load(pathToExiftool)
+
+Optional.  If you do not start exiftool with a specific path, the wrapper will start
+exiftool on first invocation of the .parse() function.  If you do not specify a path,
+the default path will be './vendor/exiftool/exiftool.exe'.
+
+###.parse(pathToFile, exiftoolArgs, callback)
+
+```pathToFile```: Full path and file name of the file from which to extract exifdata.
+
+```exiftoolArgs```: Array of string arguments to be used by exiftool.  As a rule of
+thumb, in most cases a space would indicate a new argument.  e.g. ["-j","-g","-u"].
+
+```callback(data)```: Properly scoped callback to be executed on completion.  'data' is
+the exif data in the format specified in the arguments, if any.
+
+###.unload()
+
+If you want to shut down the parser (and end the exiftool process running on the cpu),
+use this to close and delete the spawned child process.
--- exiftool.js
+++ exiftool.js
@@ -2,9 +2,14 @@ var spawn = require('child_process').spawn;
 var fs = require ('fs');
 var Q = require('q');
 
-var exif = spawn('./vendor/exiftool/exiftool.exe', ['-stay_open','True', '-@','-']);
+// Child-process variable
+var exif = {};
+// Child-process state
+var isLoaded = false;
 
+// integer (incremented) key to current exif request
 var index = 0;
+// Properly scoped callback object, properties correspond to index and callbacks
 var callbacks = {};
 
 exif.stderr.on('data', function(err) {
@@ -26,14 +31,18 @@ exif.stdout.on("data", function(data) {
   }
 });
 
-
 /**
   load the exiftool
 **/
-exports.load = function() {
+var load = function(exiftoolPath) {
+  exiftoolPath = exiftoolPath || './vendor/exiftool/exiftool.exe'
+  exif = spawn(exiftoolPath, ['-stay_open','True', '-@','-']);
+  isLoaded = true;
+};
+exports.load = load;
 
-}
 exports.parse = function(filePath,args,callback) {
+  if (!isLoaded) load();
   var deferred = Q.defer();
   var flags = '';
   for (var i = 0; i < args.length; i++) flags += args[i] + '\n';
@@ -44,7 +53,8 @@ exports.parse = function(filePath,args,callback) {
 }
 exports.parsePromise = function(file) {
   var deferred = Q.defer();
-  var flags = '-j\n-g\n-u\n'
+  var flags = '';
+  for (var i = 0; i < args.length; i++) flags += args[i] + '\n';
   var commands = flags + file.replace(/\\/g,'/') + '\n-execute' + ++index + '\n';
   //console.log(commands);
   exif.stdin.write(commands);
@@ -52,4 +62,9 @@ exports.parsePromise = function(file) {
     deferred.resolve(data);
   }
   return deferred.promise;
+}
+exports.unload = function() {
+  exif.stdin.write('stay_open\nFalse\n');
+  exif.disconnect();
+  isLoaded = false;
 }
